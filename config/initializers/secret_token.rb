6dddcfe32c99a7112423f8f95e39b1b859c6bc7b
Abraham Kuri <kurenn@icalialabs.com>
2014-02-15 14:13:38 -0600

    Inits the projects

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FurattoExample::Application.config.secret_key_base = '7bd45d15fd8167f8dc2ae8d1d3e24ae513281e946f08a41dd3df964fac9c5ee9ea616ce6b98c792237e49a10102cbc2eb363a01da396598942a4c4880cdb0f66'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FurattoExample::Application.config.session_store :cookie_store, key: '_furatto-example_session'
